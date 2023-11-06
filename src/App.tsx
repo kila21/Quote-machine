1bb385ba3296f59657e65e8aa8412cd0ca1ad003
kila <luka.kilasonia.1@btu.edu.ge>
2023-11-06 17:43:38 +0400

    display quotes and change colors randomly

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,22 @@ function App() {
   const _APIKEY = "a46de942a5msh87af6bca438e4d4p1e6cadjsn5903f4c6f6cd";
   const [data, setData] = useState<StateData | null>(null);
 
+  const colors = [
+    "rgb(231, 76, 60)",
+    "rgb(71, 46, 50)",
+    "rgb(119, 177, 169)",
+    "rgb(115, 168, 87)",
+    "rgb(243, 156, 18)",
+  ];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
   useEffect(() => {
     getNewQuote();
   }, []);
 
   const getNewQuote = () => {
+    setData(null);
+    document.documentElement.style.setProperty("--main-color", randomColor);
     axios
       .get(_API, {
         params: {
@@ -32,6 +43,11 @@ function App() {
           quote: res.data.content,
         };
         setData(newData);
+      })
+      .catch((err) => {
+        alert(
+          "Somethin went Wrong!\nMay Too Many Request Problem\nPls click New Quote Again Or Reset Page"
+        );
       });
   };
 
@@ -41,8 +57,10 @@ function App() {
         <div id="quote-box">
           {/*text and author */}
           <div className="quote-box__text">
-            <h1 id="text">{data?.quote}</h1>
-            <p id="author">
+            <h1 id="text" className={data?.quote ? "animation" : ""}>
+              {data?.quote}
+            </h1>
+            <p id="author" className={data?.name ? "animation" : ""}>
               <span>- </span> {data?.name}
             </p>
           </div>
